80b6d1c694d13a76245e757941a9c041a59c704f
RP <rp.eskandary@gmail.com>
2020-02-26 20:52:48 +0330

    Dao and entitiy added

--- app/src/main/java/com/example/project/batman/service/model/Search.kt
+++ app/src/main/java/com/example/project/batman/service/model/Search.kt
@@ -1,18 +1,29 @@
 package com.example.project.batman.service.model
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 
+
+@Entity(tableName = "tblSearch")
 class Search {
+
+    @PrimaryKey(autoGenerate = false)
+    @ColumnInfo(name = "imdbID")
+    @SerializedName("imdbID")
+    @Expose
+    var imdbID: String? = null
+
     @SerializedName("Title")
     @Expose
     var title: String? = null
     @SerializedName("Year")
     @Expose
     var year: String? = null
-    @SerializedName("imdbID")
-    @Expose
-    var imdbID: String? = null
+
+
     @SerializedName("Type")
     @Expose
     var type: String? = null
---
+++ app/src/main/java/com/example/project/batman/service/repository/room/SearchDao.kt
@@ -0,0 +1,20 @@
+package com.example.project.batman.service.repository.room
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.project.batman.service.model.Search
+
+
+@Dao
+interface SearchDao {
+
+    @Query("SELECT * FROM tblSearch")
+    fun getAll(): LiveData<List<Search>>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insertAll(lstItems: LiveData<List<Search>>)
+
+}
